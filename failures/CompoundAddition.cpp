8fd1e240b4aac85eb70e7c86dbc7f834fc63ebae
Philip Willoughby <willerz@gmail.com>
2024-10-17 15:55:25 +0100

    Add further notes on error messages for CompoundAddition

--- failures/CompoundAddition.cpp
+++ failures/CompoundAddition.cpp
@@ -11,14 +11,36 @@ int CompoundAddition(int, char** const) {
 //
 // MSVC Error messages:
 //
-// ...\RestrictedTypes\failures\CompoundAddition.cpp(6,7): error C2280: 'phil::CompoundAdditionTarget &phil::CompoundAdditionTarget::operator +=(phil::NoCompoundAdditionSource)': attempting to reference a deleted function [...\RestrictedTypes\build\failures\MixinClassFailures.vcxproj]
-//       ...\RestrictedTypes\lib\mixinclass.hpp(64,27):
+// ...\failures\CompoundAddition.cpp(6,7): error C2280: 'phil::CompoundAdditionTarget &phil::CompoundAdditionTarget::operator +=(phil::NoCompoundAdditionSource)': attempting to reference a deleted function [...\build\failures\MixinClassFailures.vcxproj]
+//       ...\lib\mixinclass.hpp(64,27):
 //       see declaration of 'phil::CompoundAdditionTarget::operator +='
-//       ...\RestrictedTypes\lib\mixinclass.hpp(64,27):
+//       ...\lib\mixinclass.hpp(64,27):
 //       'phil::CompoundAdditionTarget &phil::CompoundAdditionTarget::operator +=(phil::NoCompoundAdditionSource)': function was explicitly deleted
 //   
-// ...\RestrictedTypes\failures\CompoundAddition.cpp(6,7): error C2088: built-in operator '+=' cannot be applied to an operand of type 'phil::CompoundAdditionTarget' [...\RestrictedTypes\build\failures\MixinClassFailures.vcxproj]
+// ...\failures\CompoundAddition.cpp(6,7): error C2088: built-in operator '+=' cannot be applied to an operand of type 'phil::CompoundAdditionTarget' [...\build\failures\MixinClassFailures.vcxproj]
 //
+//
+// Clang Error messages:
+//
+// .../failures/CompoundAddition.cpp:6:7: error: overload resolution selected deleted operator '+='
+//     6 |   tgt += YesCompoundAdditionSource{2};
+//       |   ~~~ ^  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// .../lib/mixinclass.hpp:64:27: note: candidate function has been explicitly deleted
+//    64 |   CompoundAdditionTarget& operator+=(NoCompoundAdditionSource) = delete;
+//       |                           ^
+// 1 error generated.
+//
+//
+// GCC Error messages:
+//
+// .../failures/CompoundAddition.cpp: In function ‘int CompoundAddition(int, char**)’:
+// .../failures/CompoundAddition.cpp:6:37: error: use of deleted function ‘phil::CompoundAdditionTarget& phil::CompoundAdditionTarget::operator+=(phil::NoCompoundAdditionSource)’
+//     6 |   tgt += YesCompoundAdditionSource{2};
+//       |                                     ^
+// In file included from .../failures/CompoundAddition.cpp:1:
+// .../lib/mixinclass.hpp:64:27: note: declared here
+//    64 |   CompoundAdditionTarget& operator+=(NoCompoundAdditionSource) = delete;
+//       | 
 //
 //
 // clang-format: on
